38adb91ef28cff520e9f0c575b59fd2b3e5d9dd0
akshtop2025 <116068477+akshtop2025@users.noreply.github.com>
2023-08-21 17:45:57 +0530

    Add files via upload

---
+++ loginandsignpag/index.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector('.wrapper');
+const registerLink = document.querySelector('.regsiter-link');
+const loginlink = document.querySelector('.login-link');
+
+
+registerLink.onclick = () => {
+
+    wrapper.classList.add('active');
+
+}
