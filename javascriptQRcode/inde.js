83071782130ef212598fcc01aae4ed1e2e3eefc0
akshtop2025 <116068477+akshtop2025@users.noreply.github.com>
2023-08-22 17:11:01 +0530

    Add files via upload

---
+++ javascriptQRcode/inde.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5];
+const squared = numbers.map(num => num * num); // [1, 4, 9, 16, 25]
+
+console.log(squared);
+
+
+let a = '5+';
+let b = 'aksh';
+let c;
+
+c = a + b;
+console.log(a + b);
